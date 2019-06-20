9c9e65b2d423113a410110ba43bf8e9488d090be
ravishankarsr <rr@zaubacorp.com>
2019-06-20 19:17:05 +0530

    updated config details

--- main.go
+++ main.go
@@ -1,60 +1,57 @@
 package main
 
 import (
-    "fmt"
-    "time"
-     "os"
-	"os/exec"
+	"fmt"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/pkg/fields"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	"os"
 	"path/filepath"
-	"strings"
-    "k8s.io/client-go/kubernetes"
-    "k8s.io/client-go/pkg/api/v1"
-    "k8s.io/client-go/tools/cache"
-    "k8s.io/client-go/pkg/fields"
-    "k8s.io/client-go/rest"
-    "k8s.io/client-go/tools/clientcmd"
+	"time"
 )
 
 func main() {
-    var kubeconfig string
+	var kubeconfig string
 
-    // Checks for Kubectl's config file
-    if os.Getenv("KUBECONFIG") != "" {
-	kubeconfig = os.Getenv("KUBECONFIG")
-    } else {
-	kubeconfig = filepath.Join(
-	os.Getenv("HOME"), ".kube", "config",
+	// Checks for Kubectl's config file
+	if os.Getenv("KUBECONFIG") != "" {
+		kubeconfig = os.Getenv("KUBECONFIG")
+	} else {
+		kubeconfig = filepath.Join(
+			os.Getenv("HOME"), ".kube", "config",
+		)
+	}
+
+	// Generates a config object from config file
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	watchlist := cache.NewListWatchFromClient(clientset.Core().RESTClient(), "pods", v1.NamespaceDefault,
+		fields.Everything())
+	_, controller := cache.NewInformer(
+		watchlist,
+		&v1.Pod{},
+		time.Second*0,
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				fmt.Printf("add: %s \n", obj)
+			},
+			DeleteFunc: func(obj interface{}) {
+				fmt.Printf("delete: %s \n", obj)
+			},
+			UpdateFunc: func(oldObj, newObj interface{}) {
+				fmt.Printf("old: %s, new: %s \n", oldObj, newObj)
+			},
+		},
 	)
-    }
-
-    // Generates a config object from config file
-    config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-    if err != nil {
-	return nil, err
-    }
-    clientset, err := kubernetes.NewForConfig(config)
-    if err != nil {
-        panic(err.Error())
-    }
-
-    watchlist := cache.NewListWatchFromClient(clientset.Core().RESTClient(), "pods", v1.NamespaceDefault, 
-       fields.Everything())
-    _, controller := cache.NewInformer(
-        watchlist,
-        &v1.Pod{},
-        time.Second * 0,
-        cache.ResourceEventHandlerFuncs{
-            AddFunc: func(obj interface{}) {
-                fmt.Printf("add: %s \n", obj)
-            },
-            DeleteFunc: func(obj interface{}) {
-                fmt.Printf("delete: %s \n", obj)
-            },
-            UpdateFunc:func(oldObj, newObj interface{}) {
-                fmt.Printf("old: %s, new: %s \n", oldObj, newObj)
-            },
-        },
-    )
-    stop := make(chan struct{})
-    go controller.Run(stop)
+	stop := make(chan struct{})
+	go controller.Run(stop)
 }
